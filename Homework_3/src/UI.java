9dfc47b8d91012bd4e5335cdcd8984cbde4f1009
Alex Blair <ablair18@my.whitworth.edu>
2017-01-10 12:28:38 -0800

    added resources

--- Homework_3/src/UI.java
+++ Homework_3/src/UI.java
@@ -2,3 +2,7 @@
 public class UI {
 
 }
+// http://p7cdn4static.sharpschool.com/UserFiles/Servers/Server_520989/Image/school.png
+// http://classroomclipart.com/images/gallery/Clipart/Legal/police-station-with-police-cars-parked-clipart.jpg
+
+// https://s-media-cache-ak0.pinimg.com/originals/83/4e/9d/834e9dfe62b248749ff28e3304051a88.jpg
